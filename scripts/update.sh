cfe17fdd2297061eeebcb24f5d347da5274850a0
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-02-04 14:26:31 +0100

    Updated update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,10 @@
 cd ..
 git submodule foreach git pull origin master
 
-#cd bundle
-#for dir in $(find ./ -mindepth 1 -maxdepth 1 -type d);do
-    #cd $dir
-    #echo $(pwd)
-    #git submodule update --recursive
-    #cd ..
-#done
+cd bundle
+for dir in $(find ./ -mindepth 1 -maxdepth 1 -type d);do
+    cd $dir
+    echo $(pwd)
+    git submodule update --init --recursive
+    cd ..
+done
